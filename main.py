e078f3d578e01a34b41bd424797b89e700bdfe6b
Eraxty <hardikrajotiyamars@gmail.com>
2026-07-25 19:29:43 +0530

    store indexed releases in db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from nntp_client import NNTPClient
 from mapper import headers_to_articles
-from parser import group_articles
+from parser import group_articles, is_complete
+from database import create_db, save_release, get_releases
 
 host = input("Host: ")
 username = input("Username: ")
@@ -16,6 +17,8 @@ client = NNTPClient(
 )
 
 client.connect()
+
+create_db()
 
 count, first, last, name = client.select_group(group)
 
@@ -33,10 +36,16 @@ articles = headers_to_articles(headers)
 releases = group_articles(articles)
 
 for release in releases.values():
+    save_release(
+        release["name"],
+        release["size"],
+        is_complete(release)
+    )
+
     print(release["name"])
     print(f"Articles: {len(release['articles'])}")
     print(f"Size: {release['size']}")
+    print(f"Complete: {is_complete(release)}")
     print()
-
 
 client.disconnect()
